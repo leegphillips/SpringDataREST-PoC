367620edb7ee19c21ac3a2b53200e9d4be045a29
leegphillips <leegphillips@yahoo.co.uk>
2017-09-25 16:22:25 +0100

    Initial add for Spring Initializer

---
+++ src/main/java/com/github/leegphillips/springdatarestpoc/SpringDataRestPoCApplication.java
@@ -0,0 +1,12 @@
+package com.github.leegphillips.SpringDataRESTPoC;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDataRestPoCApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringDataRestPoCApplication.class, args);
+	}
+}
